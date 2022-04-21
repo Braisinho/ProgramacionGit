c8fc3b75c22a86278fbd870c5a05e3712aafa83c
Brais Ferro <braisferrooliveirafp@gmail.com>
2022-04-21 18:17:38 +0200

    Ejercicio 13 completo

--- src/main/java/ejercicioInterfacesGraficas/ejerciciosGuiados/Trece.java
+++ src/main/java/ejercicioInterfacesGraficas/ejerciciosGuiados/Trece.java
@@ -1,18 +1,75 @@
 package ejercicioInterfacesGraficas.ejerciciosGuiados;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Trece extends JFrame {
 
-    public Trece(){
+    private JPopupMenu pmen;
+    private JMenuItem menuRojo;
+    private JMenuItem menuAzul;
+    private JMenuItem menuVerde;
 
+    public Trece(){
+        super("Trece");
+        inicializar();
+        eventos();
     }
+
+    private void inicializar(){
+        pmen = new JPopupMenu();
+        menuAzul = new JMenuItem("Azul");
+        pmen.add(menuAzul);
+        menuRojo = new JMenuItem("Rojo");
+        pmen.add(menuRojo);
+        menuVerde = new JMenuItem("Verde");
+        pmen.add(menuVerde);
+    }
+
+    private void eventos(){
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton()==3) {
+                    pmen.show(Trece.this, e.getX(), e.getY());
+                }
+            }
+        });
+
+        menuRojo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getContentPane().setBackground(Color.red);
+            }
+        });
+
+        menuVerde.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getContentPane().setBackground(Color.green);
+            }
+        });
+
+        menuAzul.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getContentPane().setBackground(Color.blue);
+            }
+        });
+    }
+
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-
+                Trece ventana = new Trece();
+                ventana.setSize(500,500);
+                ventana.setVisible(true);
             }
         });
     }
